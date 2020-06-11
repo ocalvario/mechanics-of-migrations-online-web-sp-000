9e718c4ce25f6cca665ee4326e757df851baf4a9
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 16:28:05 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,19 +1,19 @@
-    mechanics-of-migrations-v-000/
-      config/
-        environment.rb
-      db/
-        migrate/
-          01_create_artists.rb
-      spec/
-        artist_spec.rb
-        spec_helper.rb
-      .gitignore
-      .learn
-      .rspec
-      artist.rb
-      CONTRIBUTING.md
-      Gemfile
-      Gemfile.lock
-      LICENSE.md
-      Rakefile
-      README.md
+class CreateArtists < ActiveRecord::Migration
+  def up
+  end
+ 
+  def down
+  end
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end 
+  end
+  
+
+
+end
